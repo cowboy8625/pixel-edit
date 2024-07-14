bb0151ad05f5e20e0ef02355b70f433dc7b057cf
cowboy8625 <cowboy8625@yahoo.com>
2024-07-14 16:30:13 -0500

    added background

--- src/Canvas.zig
+++ src/Canvas.zig
@@ -35,7 +35,14 @@ pub fn insert(self: *Self, pos: rl.Vector2(usize), color: rl.Color) !void {
     try self.pixels.put(pos, color);
 }
 
+fn drawBackground(self: *const Self) void {
+    const pos = rl.Vector2(f32).init(0, 0);
+    const size = rl.Vector2(u32).init(self.width, self.height).as(f32);
+    rl.DrawRectangleV(pos, size, rl.Color.white());
+}
+
 pub fn draw(self: *const Self, ctx: *Context) void {
+    self.drawBackground();
     const size = ctx.cursor.size.as(usize);
     var iter = self.pixels.iterator();
     while (iter.next()) |entry| {
