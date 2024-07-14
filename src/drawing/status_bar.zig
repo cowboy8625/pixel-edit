0b3a5c9a5847cec9fa861430c96d5f7cdfd57224
cowboy8625 <cowboy8625@yahoo.com>
2024-07-14 16:32:05 -0500

    fixed status bar

--- src/drawing/status_bar.zig
+++ src/drawing/status_bar.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const rl = @import("raylib_zig");
+const cast = rl.utils.cast;
 const Context = @import("../Context.zig");
 
 pub fn draw_status_bar(ctx: *Context) !void {
@@ -7,9 +8,33 @@ pub fn draw_status_bar(ctx: *Context) !void {
     const width = rl.GetScreenWidth();
     const bar_height = 20;
     const font_size = 20;
-    rl.DrawRectangle(0, height - bar_height, width, bar_height, rl.Fade(rl.Color.red(), 0.5));
-    rl.DrawText(@tagName(ctx.mode), 10, height - bar_height, font_size, rl.Color.rayWhite());
+
+    const rect_pos = rl.GetScreenToWorld2D(
+        .{
+            .x = 0.0,
+            .y = cast(f32, height - bar_height),
+        },
+        ctx.camera.*,
+    ).as(i32);
+    rl.DrawRectangle(rect_pos.x, rect_pos.y, width, bar_height, rl.Fade(rl.Color.red(), 0.5));
+
+    const mode_pos = rl.GetScreenToWorld2D(
+        .{
+            .x = 10.0,
+            .y = cast(f32, height - bar_height),
+        },
+        ctx.camera.*,
+    ).as(i32);
+    rl.DrawText(@tagName(ctx.mode), mode_pos.x, mode_pos.y, font_size, rl.Color.rayWhite());
     const text = try std.fmt.bufPrintZ(ctx.scratch_buffer, "{d}/{d}", .{ ctx.cursor.pos.x, ctx.cursor.pos.y });
     const text_size = rl.MeasureText(text, font_size);
-    rl.DrawText(text, width - 10 - text_size, height - bar_height, font_size, rl.Color.rayWhite());
+
+    const cursor_pos = rl.GetScreenToWorld2D(
+        .{
+            .x = cast(f32, width - 10 - text_size),
+            .y = cast(f32, height - bar_height),
+        },
+        ctx.camera.*,
+    ).as(i32);
+    rl.DrawText(text, cursor_pos.x, cursor_pos.y, font_size, rl.Color.rayWhite());
 }
